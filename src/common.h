acada35b24653f43131f89364b5ee02ea1379456
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-03-19 21:14:57 +0200

    Cleanup DEBUG macros

--- src/common.h
+++ src/common.h
@@ -4,21 +4,17 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <stdio.h>
 
 #define PLUGIN_NAME "uade"
 
 //#define DEBUG_TRACE 1
 
-#ifdef __cplusplus
-#include <libaudcore/runtime.h>
-#define DEBUG AUDDBG
+#ifndef __cplusplus
+# define DEBUG(fmt,...) fprintf(stderr, fmt, ## __VA_ARGS__)
 #else
-#define DEBUG(fmt,...) fprintf(stdout, fmt, ## __VA_ARGS__)
+# include <libaudcore/runtime.h>
+# define DEBUG AUDDBG
 #endif
 #define WARN(fmt,...) fprintf(stderr, fmt, ## __VA_ARGS__)
 #define ERROR(fmt,...) fprintf(stderr, fmt, ## __VA_ARGS__)
@@ -33,7 +29,4 @@ extern "C" {
 #define MODLAND_ALLMODS_MD5_FILE "modland_allmods_md5_file"
 #define PRECALC_SONGLENGTHS "precalc_songlengths"
 
-#ifdef __cplusplus
-} // extern "C"
-#endif
 #endif /* COMMON_H_ */
