7e2337162bf1aa8af6b88c5badf0eeff6662a642
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-09-15 13:34:56 +0300

    Fix GCC compile

--- src/songdb.cc
+++ src/songdb.cc
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 // Copyright (C) 2014-2023 Matti Tiainen <mvtiaine@cc.hut.fi>
 
+#include <algorithm>
 #include <fstream>
 #include <map>
 #include <numeric>
