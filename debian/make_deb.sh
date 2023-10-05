cdae2f27cf659b8b529bcd4e64ba61eeceb3b6b3
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-10-05 22:01:45 +0300

    Initial dpkg-buildpackage support

---
+++ debian/make_deb.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+apt install build-essential audacious-dev libbsd-dev autotools-dev autoconf autoconf-archive automake libtool pkg-config
+VERSION=$(cat VERSION)
+sed s/VERSION/${VERSION}/g debian/changelog.in > debian/changelog
+autoreconf -i
+./configure
+make clean
+make -j distcheck
+mv audacious-uade-${VERSION}.tar.bz2 audacious-uade-${VERSION}.orig.tar.bz2
+dpkg-buildpackage
