b228d5a476e249c89a15b3bf1a98af4b1dd89122
Matti Tiainen <mvtiaine@cc.hut.fi>
2023-11-18 11:51:43 +0200

    Add another repeat test

---
+++ src/test/precalc/test_precalc13.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname "$0")/../common/header.sh
+
+#
+
+TESTMOD="${top_srcdir}/testdata/exquisiet.ahx"
+TEST_NAME="Precalc songend - repeat2"
+TEST="${PRECALC} \"${TESTMOD}\""
+EXPECTED_OUTPUT="d2a716b5365491c0d7f082f9a438a56b	0	89705	repeat	10518"
+. $(dirname "$0")/../common/check.sh
+
+exit 0
