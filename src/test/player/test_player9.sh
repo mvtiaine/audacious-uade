fe3f6eba222dc6cc9d4eeec309f19227c5fa3ca7
Matti Tiainen <mvtiaine@cc.hut.fi>
2024-04-23 14:31:41 +0300

    ft2play: add tests

--- src/test/player/test_player9.sh
+++ src/test/player/test_player9.sh
@@ -8,10 +8,10 @@ set -e
 
 export PLAYER_ENDIAN=little
 
-TESTMOD="${top_srcdir}/testdata/MOD.forbidden pineapples"
-TESTMD5_LITTLE=71ace85fd746207ba9640441fd5f646d
+TESTMOD="${top_srcdir}/testdata/aulral.mod"
+TESTMD5_LITTLE=fc529416f9ef35a9c60f18afb5f7dc32
 
-TEST_NAME="MOD prefix"
+TEST_NAME="ft2play (FST)"
 TEST="${PLAYER} \"${TESTMOD}\" | ${MD5}"
 EXPECTED_OUTPUT=$TESTMD5_LITTLE
 . $(dirname "$0")/../common/check.sh
