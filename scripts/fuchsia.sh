2db2c828400059707ca1023cced53ae68cb71834
Matti Tiainen <mvtiaine@cc.hut.fi>
2024-07-15 11:00:44 +0300

    fuchsia: add build script

---
+++ scripts/fuchsia.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+# Fuchsia binaries not tested
+
+#autoreconf -i
+
+SYSROOT=/opt/cross/fuchsia-core/arch/arm64/sysroot \
+  CFLAGS="-target aarch64-unknown-fuchsia --sysroot ${SYSROOT}" CC=clang CXX=clang++ LD=ld.lld \
+  PATH=/opt/cross/fuchsia-clang/bin:$PATH \
+  ./configure --host=aarch64-unknown-fuchsia --with-sysroot=${SYSROOT}
+
+PATH=/opt/cross/fuchsia-clang/bin:$PATH make -j check
