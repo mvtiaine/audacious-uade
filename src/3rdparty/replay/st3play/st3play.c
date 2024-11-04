f484ad4c78b233b52375aea91a73dd6548610e28
Matti Tiainen <mvtiaine@cc.hut.fi>
2024-11-04 17:53:45 +0200

    st3play: fix mix buffer overflow with 96khz

--- src/3rdparty/replay/st3play/st3play.c
+++ src/3rdparty/replay/st3play/st3play.c
@@ -140,7 +140,7 @@ uint32_t st3play_GetMixerTicks(void); // returns the amount of milliseconds of m
 
 //#define USE_ST3_GUS_TEMPO
 
-#define MIX_BUF_SAMPLES 4096
+#define MIX_BUF_SAMPLES 8192 // mvtiaine: increased buf size to avoid overflow with freq 96000
 
 #ifndef AUDACIOUS_UADE
 #include <stdio.h>
