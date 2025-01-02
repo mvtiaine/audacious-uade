efe6d2de1512210be06ab69f81b3fd0a0a6c1814
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-02 21:53:36 +0200

    hivelytracker: add another test hash

--- src/test/player/test_player_hivelytracker_1.sh
+++ src/test/player/test_player_hivelytracker_1.sh
@@ -15,13 +15,14 @@ TEST_NAME="HivelyTracker"
 TEST="${PLAYER} \"${TESTMOD}\" | ${MD5}"
 EXPECTED_OUTPUT=$TESTMD5_LITTLE
 
-# XXX on x86/68k/... (32-bit) hosts HivelyTracker replay produces slightly different output vs 64-bit hosts
+# XXX HivelyTracker replay may produce slightly different output depending on host
 # TODO figure out root cause
 OUTPUT=$(eval ${TEST})
 TEST="echo ${OUTPUT}"
 if [ "$OUTPUT" = "db12f1eaf5a14ec73cc4e45a56086936" ]; then
-    # 32-bit
     EXPECTED_OUTPUT=db12f1eaf5a14ec73cc4e45a56086936
+elif [ "$OUTPUT" = "f0c3b8b358b3d70d00b1caa19f1bc92c" ]; then
+    EXPECTED_OUTPUT=f0c3b8b358b3d70d00b1caa19f1bc92c
 fi
 
 . $(dirname "$0")/../common/check.sh
