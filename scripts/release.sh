77a3297bae0fc6a753370b94b7482dd048118a34
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-23 17:46:23 +0200

    Update release.sh

--- scripts/release.sh
+++ scripts/release.sh
@@ -7,7 +7,7 @@ cp VERSION-RELEASE VERSION
 autoreconf -i && \
   ./configure && \
   make clean && \
-  CFLAGS="-Wall -Werror" \
+  CFLAGS="-Wall -Wpedantic -Wextra -Werror" \
   CXXFLAGS="${CFLAGS}" \
     make -j distcheck
 echo ${VERSION_OLD} > VERSION
