982e0a8792302c3abab5612b7e34f8071aea5096
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-23 16:28:50 +0200

    protrekkr: fix loading of NoiseTrekker 2 & ProTrekkr 1 files
    https://github.com/hitchhikr/protrekkr/issues/17

--- src/3rdparty/replay/protrekkr/src/files/synths.cpp
+++ src/3rdparty/replay/protrekkr/src/files/synths.cpp
@@ -55,7 +55,7 @@ void Read_Synth_Params(int (*Read_Function)(void *, int ,int, FILE *),
     {
         if(read_disto && read_lfo_adsr)
         {
-            Read_Function(&PARASynth[idx], sizeof(Synth_Parameters), 1, in);
+            Read_Function(&PARASynth[idx], sizeof(Synth_Parameters) - 4, 1, in); // mvtiaine: https://github.com/hitchhikr/protrekkr/issues/17
             PARASynth[idx].disto /= 2;
             PARASynth[idx].disto += 64;
             PARASynth[idx].glb_volume = 0x7f;
@@ -64,7 +64,7 @@ void Read_Synth_Params(int (*Read_Function)(void *, int ,int, FILE *),
         {
             if(read_disto)
             {
-                Read_Function(&PARASynth[idx], sizeof(Synth_Parameters) - 32, 1, in);
+                Read_Function(&PARASynth[idx], sizeof(Synth_Parameters) - 32 - 4, 1, in); // mvtiaine: https://github.com/hitchhikr/protrekkr/issues/17
             }
             else
             {
@@ -157,7 +157,7 @@ void Read_Synth_Params(int (*Read_Function)(void *, int ,int, FILE *),
                 }
                 else
                 {
-                    Read_Function(&PARASynth[idx], sizeof(Synth_Parameters) - 4 - 32, 1, in);
+                    Read_Function(&PARASynth[idx], sizeof(Synth_Parameters) - 4 - 32 - 4, 1, in); // mvtiaine: https://github.com/hitchhikr/protrekkr/issues/17
                 }
             }
         }
