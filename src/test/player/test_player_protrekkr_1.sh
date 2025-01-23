d884ad84809e5e07a7b76304ce3bc2009de669e2
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-23 23:03:19 +0200

    protrekkr: moar test hashes

--- src/test/player/test_player_protrekkr_1.sh
+++ src/test/player/test_player_protrekkr_1.sh
@@ -30,6 +30,8 @@ elif [ "$OUTPUT" = "d665ca5c94c3f822819420148e550b17" ]; then
     EXPECTED_OUTPUT=d665ca5c94c3f822819420148e550b17
 elif [ "$OUTPUT" = "9f2ef0c6aaaf5b1b60878e5ce073e729" ]; then
     EXPECTED_OUTPUT=9f2ef0c6aaaf5b1b60878e5ce073e729
+elif [ "$OUTPUT" = "1a5617ed2ff0c848d27bc4f1292005ba" ]; then
+    EXPECTED_OUTPUT=1a5617ed2ff0c848d27bc4f1292005ba
 fi
 
 . $(dirname "$0")/../common/check.sh
