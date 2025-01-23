613a183d3786bd316852270d2598c7208b59ca9f
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-01-23 16:41:30 +0200

    protrekkr: add test

---
+++ src/test/player/test_player_protrekkr_1.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+. $(dirname "$0")/../common/header.sh
+
+#
+
+export PLAYER_ENDIAN=little
+
+TESTMOD="${top_srcdir}/testdata/elevator zax.ptk"
+TESTMD5_LITTLE=a5917bb69627854e837ba92db6aaf033
+
+TEST_NAME="ProTrekkr 2"
+TEST="${PLAYER} \"${TESTMOD}\" | ${MD5}"
+EXPECTED_OUTPUT=$TESTMD5_LITTLE
+. $(dirname "$0")/../common/check.sh
+
+exit 0
