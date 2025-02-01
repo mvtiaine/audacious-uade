29476379274672d4799c5ceab76b0cd0f5356495
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-02-01 20:46:15 +0200

    hivelytracker: use precalc for test

--- src/test/player/test_player_hivelytracker_1.sh
+++ src/test/player/test_player_hivelytracker_1.sh
@@ -8,24 +8,13 @@ player=hivelytracker
 
 #
 
-export PLAYER_ENDIAN=little
+# XXX using precalc as HivelyTracker replay may produce slightly different output depending on host
+# TODO figure out root cause
 
 TESTMOD="${top_srcdir}/testdata/monkmusings.hvl"
-TESTMD5_LITTLE=f52dea21bd60cf27f0e529666fad05aa
-
 TEST_NAME="HivelyTracker"
-TEST="${PLAYER} \"${TESTMOD}\" | ${MD5}"
-EXPECTED_OUTPUT=$TESTMD5_LITTLE
-
-# XXX HivelyTracker replay may produce slightly different output depending on host
-# TODO figure out root cause
-OUTPUT=$(eval ${TEST})
-TEST="echo ${OUTPUT}"
-if [ "$OUTPUT" = "db12f1eaf5a14ec73cc4e45a56086936" ]; then
-    EXPECTED_OUTPUT=db12f1eaf5a14ec73cc4e45a56086936
-elif [ "$OUTPUT" = "f0c3b8b358b3d70d00b1caa19f1bc92c" ]; then
-    EXPECTED_OUTPUT=f0c3b8b358b3d70d00b1caa19f1bc92c
-fi
+TEST="${PRECALC} \"${TESTMOD}\""
+EXPECTED_OUTPUT="ce594632803f682e9c3c644087b4e58b	0	45991	player	hivelytracker	HivelyTracker	7	2378"
 
 . $(dirname "$0")/../common/check.sh
 
