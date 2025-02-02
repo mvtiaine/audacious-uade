abe826663cb8adcbb2caa8f5df8d752e382a3c33
Matti Tiainen <mvtiaine@cc.hut.fi>
2025-02-02 19:50:36 +0200

    it2play: fix

--- src/3rdparty/replay/it2play/loaders/s3m.c
+++ src/3rdparty/replay/it2play/loaders/s3m.c
@@ -471,7 +471,7 @@ static bool GetPatternLength(uint16_t Rows, uint16_t *LengthOut)
 				}
 			}
 
-			uint16_t EfxAndParam = (uint16_t)((uint8_t)Src[3] | ((uint8_t)Src[4] << 8)); // mvtiaine: fixed UB
+			uint16_t EfxAndParam = ((uint16_t)Src[4] << 8) | Src[3]; // mvtiaine: fixed UB
 			if (EfxAndParam != 0)
 			{
 				if (*(uint16_t *)&Enc[4] != EfxAndParam)
@@ -562,7 +562,7 @@ static void EncodePattern(pattern_t *p, uint8_t Rows)
 				}
 			}
 
-			uint16_t EfxAndParam = (uint16_t)((uint8_t)Src[4] << 8) | (uint8_t)Src[3]; // mvtiaine: fixed UB
+			uint16_t EfxAndParam = ((uint16_t)Src[4] << 8) | Src[3]; // mvtiaine: fixed UB
 			if (EfxAndParam != 0)
 			{
 				if (EfxAndParam != *(uint16_t *)&Enc[4])
